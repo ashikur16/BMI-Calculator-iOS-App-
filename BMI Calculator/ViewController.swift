c374b92b920d4a288e54ab7971be29ccb4e849fd
Md. Ashikur Rahman <EWUashikurrahman@gmail.com>
2022-09-23 18:05:15 +0600

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Ashikur Rahman on 23/09/2022.
+//  Copyright © 2022 Ashikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BMI CalculatorTests/BMI_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  BMI_CalculatorTests.swift
+//  BMI CalculatorTests
+//
+//  Created by Ashikur Rahman on 23/09/2022.
+//  Copyright © 2022 Ashikur Rahman. All rights reserved.
+//
+
+import XCTest
+@testable import BMI_Calculator
+
+class BMI_CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
